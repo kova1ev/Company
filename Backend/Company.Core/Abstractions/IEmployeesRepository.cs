4bc114525710c41db30c01cc0eaaf26e2b81887e
kovalev <kov92av@gmail.com>
2024-01-21 20:26:16 +0300

    add Employee and repo interface

---
+++ Backend/Company.Core/Abstractions/IEmployeesRepository.cs
@@ -0,0 +1,11 @@
+﻿using Company.Core.Domain;
+
+namespace Company.Core.Abstractions;
+public interface IEmployeesRepository
+{
+    Task<Guid> Create(Employee employee);
+    Task<Guid> Delete(Guid id);
+    Task<IEnumerable<Employee>> Get();
+    Task<Employee?> GetById(Guid id);
+    Task<Guid> Update(Employee employee);
+}
---
+++ Backend/Company.Core/Domain/Employee.cs
@@ -0,0 +1,21 @@
+﻿namespace Company.Core.Domain;
+
+public class Employee
+{
+    public Employee(Guid id, string department, string fullName, decimal salary, DateOnly dateOfBirth, DateOnly employmentDate)
+    {
+        Id = id;
+        Department = department;
+        FullName = fullName;
+        Salary = salary;
+        DateOfBirth = dateOfBirth;
+        EmploymentDate = employmentDate;
+    }
+
+    public Guid Id { get; }
+    public string Department { get; }
+    public string FullName { get; }
+    public decimal Salary { get; }
+    public DateOnly DateOfBirth { get; }
+    public DateOnly EmploymentDate { get; }
+}
